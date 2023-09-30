1e802af5cd7fe9002a1f95132208a05f53eff693
Jan E <jan.elling.h@gmail.com>
2023-09-30 05:33:41 +0200

    Update discord_funcs.js

--- functions/discord_funcs.js
+++ functions/discord_funcs.js
@@ -1,1 +1,14 @@
+async function message_delete(message_data, time){
+    /** 
+     * Deletes a message, within set time.
+     * @param {message_data} Promise with channelId and Id (MessageId)
+     * @param {time} int in milliseconds
+    */
+    message_data.then( async(data) => {
+      channel = await client.channels.fetch(data.channelId)
+      setTimeout(() => channel.messages.delete(data.id), time)
+    })
+}
 
+
+module.exports = {message_delete}
